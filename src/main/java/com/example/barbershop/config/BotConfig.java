ad030ac3d5c9fe0da87310450209dcc3a7a7bfd0
Danil <dplotnikov987@gmail.com>
2026-02-18 17:46:54 +0400

    telegram bot init

---
+++ src/main/java/com/example/barbershop/config/BotConfig.java
@@ -0,0 +1,20 @@
+package com.example.barbershop.config;
+
+import com.example.barbershop.bot.BarbershopBot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(BarbershopBot bot) throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+        return api;
+    }
+
+}
